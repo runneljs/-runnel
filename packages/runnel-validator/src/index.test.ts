52f1f3363b0a586642d41167b793070ea43e3f20
wataru <wataru.oguchi.0to1@gmail.com>
2024-03-07 16:26:04 -0800

    Make the schema optional
    When no schema is given, the validator checks whether the given payload is null or undefined.

--- packages/runnel-validator/src/index.test.ts
+++ packages/runnel-validator/src/index.test.ts
@@ -2,17 +2,36 @@ import { beforeAll, describe, expect, test } from "bun:test";
 import { validator } from "./index";
 
 describe("varidator", () => {
-  let validate: (payload: unknown) => boolean;
+  describe("with a schema", () => {
+    let validate: ReturnType<typeof validator>;
 
-  beforeAll(() => {
-    validate = validator({ type: "object" });
+    beforeAll(() => {
+      validate = validator({ type: "object" });
+    });
+
+    test("should return true for valid payload", () => {
+      expect(validate({})).toBe(true);
+    });
+
+    test("should return false for invalid payload", () => {
+      expect(validate("")).toBe(false);
+    });
   });
 
-  test("should return true for valid payload", () => {
-    expect(validate({})).toBe(true);
-  });
+  describe("without a schema", () => {
+    let validate: ReturnType<typeof validator>;
 
-  test("should return false for invalid payload", () => {
-    expect(validate("")).toBe(false);
+    beforeAll(() => {
+      validate = validator();
+    });
+
+    test("should return false for a payload", () => {
+      expect(validate({})).toBe(false);
+    });
+
+    test("should return true for (null/undefined) payload", () => {
+      expect(validate()).toBe(true);
+      expect(validate(null)).toBe(true);
+    });
   });
 });
--- packages/runnel-validator/src/index.ts
+++ packages/runnel-validator/src/index.ts
@@ -1,8 +1,11 @@
 import { Validator } from "@cfworker/json-schema";
 
-export function validator(jsonSchema: object) {
+export function validator(jsonSchema?: object): (payload?: unknown) => boolean {
+  if (!jsonSchema)
+    return (payload?: unknown) =>
+      typeof payload === "undefined" || payload === null;
   const validator = new Validator(jsonSchema);
-  return function (payload: unknown) {
+  return function (payload?: unknown) {
     return validator.validate(payload).valid;
   };
 }
